7df6b76dcbb9c4d7019fab1f49e5f7ad5e087066
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2003-11-08 13:05:42 +0000

    add some files, default.schema, qterm.prj, trayicon_mac.cpp
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk@46 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

---
+++ qterm/qterm/trayicon_mac.cpp
@@ -0,0 +1,19 @@
+#include "trayicon.h"
+
+#ifdef Q_OS_MACX 
+void TrayIcon::sysInstall()
+{
+}
+
+void TrayIcon::sysRemove()
+{
+}
+
+void TrayIcon::sysUpdateIcon()
+{
+}
+
+void TrayIcon::sysUpdateToolTip()
+{
+}
+#endif // Q_OS_MACX 
