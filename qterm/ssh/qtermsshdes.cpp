d4c259537747872f5a28eb79f673ccf2dc3aa6be
hephooey <hephooey@users.noreply.github.com>
2004-02-20 04:57:44 +0000

    SSH support
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm@56 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

---
+++ qterm/ssh/qtermsshdes.cpp
@@ -0,0 +1,61 @@
+#include "qtermsshconst.h"
+#include "qtermsshdes.h"
+
+#include <memory.h>
+#include <openssl/des.h>
+
+#define DES_ENCRYPT 1
+#define DES_DECRYPT 0
+
+//==============================================================================
+//QTermSSH1DES3
+//==============================================================================
+
+QTermSSH1DES3::QTermSSH1DES3()
+{
+	d_name = "des3-cbc";
+	d_blockSize = 8;
+	d_keyBits = 16;
+	d_code = SSH_CIPHER_3DES;
+
+	memset(d_IV1, 0, sizeof(d_IV1));
+	memset(d_IV2, 0, sizeof(d_IV2));
+	memset(d_IV3, 0, sizeof(d_IV3));
+}
+
+void QTermSSH1DES3::decrypt(const u_char * source, u_char * dest, int len)
+{
+	DES_ncbc_encrypt(source, dest, len, &d_key3, &d_IV3, DES_DECRYPT);
+	DES_ncbc_encrypt(dest, dest, len, &d_key2, &d_IV2, DES_ENCRYPT);
+	DES_ncbc_encrypt(dest, dest, len, &d_key1, &d_IV1, DES_DECRYPT);
+}
+
+void QTermSSH1DES3::encrypt(const u_char * source, u_char * dest, int len)
+{
+	DES_ncbc_encrypt(source, dest, len, &d_key1, &d_IV1, DES_ENCRYPT);
+	DES_ncbc_encrypt(dest, dest, len, &d_key2, &d_IV2, DES_DECRYPT);
+	DES_ncbc_encrypt(dest, dest, len, &d_key3, &d_IV3, DES_ENCRYPT);
+}
+
+void QTermSSH1DES3::setIV(const u_char * data)
+{
+	memset(d_IV1, 0, sizeof(d_IV1));
+	memset(d_IV2, 0, sizeof(d_IV2));
+	memset(d_IV3, 0, sizeof(d_IV3));
+}
+
+void QTermSSH1DES3::setKey(const u_char * data)
+{
+	DES_cblock key;
+	memset(key, 0, sizeof(key));
+	memcpy(key, data, sizeof(key));
+	DES_set_key(&key, &d_key1);
+	data += 8;
+	memset(key, 0, sizeof(key));
+	memcpy(key, data, sizeof(key));
+	DES_set_key(&key, &d_key2);
+	data += 8;
+	memset(key, 0, sizeof(key));
+	memcpy(key, data, sizeof(key));
+	DES_set_key(&key, &d_key3);
+}
---
+++ qterm/ssh/qtermsshsession.h
@@ -0,0 +1,58 @@
+#ifndef QTERMSSHSESSION_H
+#define QTERMSSHSESSION_H
+
+#include <sys/types.h>
+#include <qobject.h>
+
+class QTermSSHPacketReceiver;
+class QTermSSHPacketSender;
+
+class QTermSSHSession : public QObject
+{
+	Q_OBJECT
+protected:
+	bool d_closed;
+	
+	QTermSSHPacketReceiver * d_incomingPacket;
+	QTermSSHPacketSender * d_outcomingPacket;
+	
+public:
+	QTermSSHSession()
+	{
+	}
+	virtual ~QTermSSHSession()
+	{
+	}
+	virtual void initSession(QTermSSHPacketReceiver * packet, QTermSSHPacketSender * output) = 0;
+	virtual void closeConnection(char * reason) = 0;
+	// virtual void handleDisconntion(int reason);
+	virtual void changeWindowSize(int col, int row) = 0;
+	
+public slots:
+	virtual void handlePacket(int type) = 0;
+signals:
+	void sessionOK();
+	void readyRead();
+};
+
+class QTermSSH1Session : public QTermSSHSession
+{
+	Q_OBJECT
+private:
+	enum QTermSSH1SessionState {
+		BEGIN_SERVICE,
+		REQPTY_SENT,
+		//REQCMD_SENT,
+		SERVICE_OK
+	} d_serviceState;
+	
+public:
+	QTermSSH1Session();
+	void initSession(QTermSSHPacketReceiver * packet, QTermSSHPacketSender * output);
+	void closeConnection(char * reason);
+	void changeWindowSize(int col, int row);
+public slots:
+	void handlePacket(int type);
+};
+
+#endif		//QTERMSSHSESSION_H
