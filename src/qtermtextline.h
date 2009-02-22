d41fc01f3ed0bd12d2cf3b978f7be815e0465be2
hephooey <hephooey@users.noreply.github.com>
2009-02-22 21:04:55 +0000

    Fix indentation, remove some comments
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@610 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

--- src/qtermtextline.h
+++ src/qtermtextline.h
@@ -11,51 +11,59 @@ class TextLine
 {
 
 public:
-	TextLine();
-	~TextLine();
+    TextLine();
+    ~TextLine();
 
-	void reset();
+    void reset();
 
-	void setChanged(int start, int end); 
-	
-	void clearChange() { m_bChanged = false; m_start=-1; m_end=-1; }
+    void setChanged(int start, int end);
 
-	bool isChanged( int &start, int &end );
-	
-	QByteArray getColor() { return m_color; }
+    void clearChange() {
+        m_bChanged = false; m_start = -1; m_end = -1;
+    }
 
-	QByteArray getAttr() { return m_attr; }
+    bool isChanged(int &start, int &end);
 
-	int getLength()	{ return m_length; }
+    QByteArray getColor() {
+        return m_color;
+    }
 
-	QByteArray getAttrText( int index = -1, int len = -1, const QByteArray & escape="\x1b\x1b" );
+    QByteArray getAttr() {
+        return m_attr;
+    }
 
-	QByteArray getText( int index = -1, int len = -1 );
+    int getLength() {
+        return m_length;
+    }
 
-	void insertText( const QByteArray& str, short attr = -1, int index = -1 );
+    QByteArray getAttrText(int index = -1, int len = -1, const QByteArray & escape = "\x1b\x1b");
 
-	void deleteText( int index = -1, int len = -1 );
-	
-	void replaceText( const QByteArray& str, short attr = -1, int index = -1, int len = -1 );
+    QByteArray getText(int index = -1, int len = -1);
 
-	bool hasBlink();
+    void insertText(const QByteArray& str, short attr = -1, int index = -1);
+
+    void deleteText(int index = -1, int len = -1);
+
+    void replaceText(const QByteArray& str, short attr = -1, int index = -1, int len = -1);
+
+    bool hasBlink();
 
 protected:
 
 
-	// we use QString to store any character after decode
-	int m_length;
-	QByteArray m_text;
-	QByteArray m_color;
-	QByteArray m_attr;
-	char m_curColor;
-	char m_curAttr;
-	bool m_bChanged;
-	bool m_bBlink;
+    // we use QString to store any character after decode
+    int m_length;
+    QByteArray m_text;
+    QByteArray m_color;
+    QByteArray m_attr;
+    char m_curColor;
+    char m_curAttr;
+    bool m_bChanged;
+    bool m_bBlink;
 
-	int m_start, m_end;
+    int m_start, m_end;
 };
 
 } // namespace QTerm
 
-#endif	//QTERMTEXTLINE_H
+#endif //QTERMTEXTLINE_H
