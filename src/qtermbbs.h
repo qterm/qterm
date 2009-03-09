50b7e58dc4c38c46e77ce4d69fd594fa6496ca1d
hephooey <hephooey@users.noreply.github.com>
2009-03-09 14:19:02 +0000

    Format files
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@681 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

--- src/qtermbbs.h
+++ src/qtermbbs.h
@@ -16,46 +16,46 @@ class Buffer;
 class BBS
 {
 public:
-	BBS( Buffer * );
-	~BBS();
+    BBS(Buffer *);
+    ~BBS();
 
-	/* -1 -- undefined
-	 *  0 -- menu
-	 *  1 -- article or board list
-	 *  2 -- read
-	 *  3 -- edit
-	 */
-	void setPageState();
-	void setScreenStart(int);
-	bool setCursorPos( const QPoint&, QRect& );
-			
-	bool isSelected( int );
-	bool isSelected( const QPoint& );
-	bool isUrl(QRect&,QRect&);
-	bool isIP(QRect&, QRect&);
-	bool isPageComplete();
-	bool checkUrl(QRect&, QRect&, bool);
+    /* -1 -- undefined
+     *  0 -- menu
+     *  1 -- article or board list
+     *  2 -- read
+     *  3 -- edit
+     */
+    void setPageState();
+    void setScreenStart(int);
+    bool setCursorPos(const QPoint&, QRect&);
 
-	QString getUrl();
-	QString getIP();
-	int getPageState();
-	char getMenuChar();
-	QRect getSelectRect();
-	int getCursorType(const QPoint&);
-	QString getMessage();
-	
+    bool isSelected(int);
+    bool isSelected(const QPoint&);
+    bool isUrl(QRect&, QRect&);
+    bool isIP(QRect&, QRect&);
+    bool isPageComplete();
+    bool checkUrl(QRect&, QRect&, bool);
+
+    QString getUrl();
+    QString getIP();
+    int getPageState();
+    char getMenuChar();
+    QRect getSelectRect();
+    int getCursorType(const QPoint&);
+    QString getMessage();
+
 protected:
-	bool isUnicolor( TextLine * );
-	bool isIllChar(char);
-	Buffer *m_pBuffer;
+    bool isUnicolor(TextLine *);
+    bool isIllChar(char);
+    Buffer *m_pBuffer;
 
-	QRect m_rcUrl;
-	QString m_strUrl;
-	QString m_strIP;
-	char m_cMenuChar;
-	int m_nPageState;
-	QPoint m_ptCursor;
-	int m_nScreenStart;
+    QRect m_rcUrl;
+    QString m_strUrl;
+    QString m_strIP;
+    char m_cMenuChar;
+    int m_nPageState;
+    QPoint m_ptCursor;
+    int m_nScreenStart;
 };
 
 } // namespace QTerm
