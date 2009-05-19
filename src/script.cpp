08c9fb3c2cca6747d32ceec01da37b40a1273753
hephooey <hephooey@users.noreply.github.com>
2009-05-19 22:12:51 +0000

    Resurrect the script support, now with QtScript
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@737 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

---
+++ src/script.cpp
@@ -0,0 +1,71 @@
+#include "qterm.h"
+#include "script.h"
+#include "qtermwindow.h"
+#include "qtermbuffer.h"
+#include "qtermscreen.h"
+#include "qtermtextline.h"
+
+namespace QTerm
+{
+Script::Script(Window * parent)
+    :QObject(parent)
+{
+    m_window = parent;
+}
+
+Script::~Script()
+{
+}
+
+int Script::caretX()
+{
+    return m_window->m_pBuffer->caret().x();
+}
+
+int Script::caretY()
+{
+    return m_window->m_pBuffer->caret().y();
+}
+
+int Script::columns()
+{
+    return m_window->m_pBuffer->columns();
+}
+
+int Script::rows()
+{
+    return m_window->m_pBuffer->line();
+}
+
+void Script::sendString(const QString & string)
+{
+    m_window->inputHandle(string);
+}
+
+void Script::showMessage(const QString & message)
+{
+    m_window->showMessage(message);
+}
+
+QString Script::getText(int line)
+{
+    return m_window->m_pBuffer->screen(line)->getText();
+}
+
+QString Script::getAttrText(int line)
+{
+    return m_window->m_pBuffer->screen(line)->getAttrText();
+}
+
+QByteArray Script::getColor(int line)
+{
+    return m_window->m_pBuffer->screen(line)->getColor();
+}
+
+QByteArray Script::getAttr(int line)
+{
+    return m_window->m_pBuffer->screen(line)->getAttr();
+}
+
+} // namespace QTerm
+#include <script.moc>
---
+++ src/script.h
@@ -0,0 +1,31 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+#include <QtCore/QObject>
+
+namespace QTerm
+{
+class Window;
+class Script : public QObject
+{
+    Q_OBJECT
+public:
+    Script(Window *parent);
+    ~Script();
+public slots:
+    int caretX();
+    int caretY();
+    int columns();
+    int rows();
+    void sendString(const QString & string);
+    void showMessage(const QString & message);
+//    void sendParsedString(const QString & message);
+    QString getText(int line);
+    QString getAttrText(int line);
+    QByteArray getColor(int line);
+    QByteArray getAttr(int line);
+private:
+    Window * m_window;
+};
+} // namespace QTerm
+
+#endif //SCRIPT_H
