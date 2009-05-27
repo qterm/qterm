bbf8a4621eaa8e3238e13fd4a226b3b5d817e5cf
hephooey <hephooey@users.noreply.github.com>
2009-05-27 05:14:19 +0000

    We do not need fromUtf8 anymore, also simplify the functions a little bit
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@785 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

--- src/scripts/smth.js
+++ src/scripts/smth.js
@@ -17,39 +17,15 @@ function getBG(color)
     return bg;
 }
 
-function fromUtf8(utftext)
+String.prototype.startsWith = function(list)
 {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-
-    while ( i < utftext.length ) {
-
-        c = utftext.charCodeAt(i);
-
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
-        }
-        else if((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i+1);
-            c3 = utftext.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
-
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        if (this.substr(0,item.length) == item)
+            return true;
     }
-
-    return string;
-}
-
-String.prototype.startsWith = function(str) {
-    return(this.substr(0,str.length) == str);
+    return false;
+    //return(this.substr(0,str.length) == str);
 }
 
 String.prototype.contains = function(str) {
@@ -66,33 +42,18 @@ function setPageState()
     var title = getText(0);
     var bottom = getText(QTerm.rows()-1);
     pageState = -1;
-    if (title.startsWith(fromUtf8("主选单")))
+    var menuList = ["主选单","聊天选单","[处理信笺选单]","工具箱选单","分类讨论区选单"];
+    var listList = ["版主:","[好朋友列表]","[讨论区列表]","邮件选单","[个人定制区]"];
+    var articleList = ["[阅读文章]","下面还有喔","(R)回信, (D)删除, (G)继续? [G]:"];
+    if (title.startsWith(menuList))
         pageState = 0;
-    if (title.startsWith(fromUtf8("聊天选单")))
-        pageState = 0;
-    if (title.startsWith(fromUtf8("[处理信笺选单]")))
-        pageState = 0;
-    if (title.startsWith(fromUtf8("工具箱选单")))
-        pageState = 0;
-    if (title.startsWith(fromUtf8("版主:")))
+    if (title.startsWith(listList))
         pageState = 1;
-    if (title.startsWith(fromUtf8("[好朋友列表]")))
+    if (title.indexOf("水木社区 精华区公布栏")!=-1)
         pageState = 1;
-    if (title.startsWith(fromUtf8("[讨论区列表]")))
+    if (title.indexOf("本日十大热门话题")!=-1)
         pageState = 1;
-    if (title.startsWith(fromUtf8("邮件选单")))
-        pageState = 1;
-    if (title.startsWith(fromUtf8("[个人定制区]")))
-        pageState = 1;
-    if (title.contains(fromUtf8("水木社区 精华区公布栏")))
-        pageState = 1;
-    if (title.contains(fromUtf8("本日十大热门话题")))
-        pageState = 1;
-    if (bottom.startsWith(fromUtf8("[阅读文章]")))
-        pageState = 2;
-    if (bottom.startsWith(fromUtf8("下面还有喔")))
-        pageState = 2;
-    if (bottom.startsWith(fromUtf8("(R)回信, (D)删除, (G)继续? [G]:")))
+    if (bottom.startsWith(articleList))
         pageState = 2;
     return pageState;
 }
