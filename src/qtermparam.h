1d50e477a0d5ff28839612948935d34309aa65de
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2010-07-21 20:05:00 +0000

    forgot commit
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@1206 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

--- src/qtermparam.h
+++ src/qtermparam.h
@@ -6,6 +6,8 @@
 
 #include <QString>
 #include <QColor>
+#include <QVariant>
+
 namespace QTerm
 {
 class Param;
@@ -19,93 +21,8 @@ public:
 	~Param();
 
 	Param& operator=(const Param&);
-// General
-	// Name
-	QString m_strName;
-	// Address
-	QString m_strAddr;
-	// Port
-	quint16 m_uPort;
-	// Host Type	
-	int m_nHostType;	// 0--BBS 1--*NIX
-	// Auto Login
-	bool m_bAutoLogin;
-	// Pre Login
-	QString m_strPreLogin;
-	// User Name
-	QString m_strUser;
-	// Password
-	QString m_strPasswd;
-	// Post Login
-	QString m_strPostLogin;
-// Display
-	 // 0--GBK  1--BGI5
-	QString m_BBSCode;
-	// 0--GBK 1--BIG5
-    int m_nDispCode;
-	// Auto Change Font When Window Resized
-	bool m_bAutoFont;
-	// Always Highlight
-	bool m_bAlwaysHighlight;
-	// ANSI Color
-	bool m_bAnsiColor;
-	// Font Name
-	QString m_strASCIIFontName;
-	QString m_strGeneralFontName;
-	// Font Size
-	int m_nFontSize;
-	// Scheme File
-	QString m_strSchemeFile;
-// Terminal
-	// Terminal Type
-	QString m_strTerm;
-	// Keyboard Profile
-	QString m_strKeyboardProfile;
-	// Columns & Rows
-	int m_nCol, m_nRow;
-	// Scroll Lines
-	int m_nScrollLines;
-	// Curor Type
-	int m_nCursorType; // 0--Block 1--Underline 2--I Type
-	// the esacpe string
-	QString m_strEscape;
-// Connection
-	// Proxy Type
-	int m_nProxyType; // 0--None 1--Wingate 2--SOCKS4 3--SOCKS5 4--HTTP
-	// Address
-	QString m_strProxyHost;
-	// Port
-	quint16 m_uProxyPort;
-	// Authentation
-	bool m_bAuth;
-	// User Name
-	QString m_strProxyUser;
-	// Password
-	QString m_strProxyPasswd;
-	// Protocol
-	int m_nProtocolType; // 0--Telnet 1--SSH1 2--SSH2
-// Misc
-	// Max Idle Time %s
-	int m_nMaxIdle;
-	QString m_strReplyKey;
-	// Send When Idle
-	QString m_strAntiString;
-	// wether autoreply
-	bool m_bAutoReply;
-	// Auto Reply Messages
-	QString m_strAutoReply;
-	// Reconnect When Disconnected By Host
-	bool m_bReconnect;
-	// Reconnect Interval (s)
-	int m_nReconnectInterval;
-	// Retry times
-	int m_nRetry;	// -1 -- infinite
-// Mouse
-	int m_nMenuType;	// 0--underline 1--reverse 2--color
-	QColor m_clrMenu;
-// Script
-	bool m_bLoadScript;
-	QString m_strScriptFile;
+
+	QVariantMap m_mapParam;
 };
 
 } // namespace QTerm
