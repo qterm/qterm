76ef848798cc215e8145976008ba5529173d5ecb
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2010-08-09 10:03:20 +0000

    a test case for http redirection
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@1238 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

---
+++ src/test/http/main.cpp
@@ -0,0 +1,12 @@
+#include "qnamredirect.h"
+
+#include <QtGui>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QNAMRedirect w;
+    w.show();
+    return a.exec();
+}
