57857d89aa32817c3213c74ce3e311c55400e1d0
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2011-10-16 18:58:15 +0000

    apple dargndrop package
    git-svn-id: https://svn.code.sf.net/p/qterm/code/trunk/qterm-qt4@1298 bd9efd21-7b0e-0410-bb59-ddbc1b2ac886

--- package.cmake
+++ package.cmake
@@ -8,17 +8,19 @@ SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYRIGHT")
 SET(CPACK_PACKAGE_VERSION_MAJOR ${QTERM_VERSION_MAJOR})
 SET(CPACK_PACKAGE_VERSION_MINOR ${QTERM_VERSION_MINOR})
 SET(CPACK_PACKAGE_VERSION_PATCH ${QTERM_VERSION_RELEASE})
-IF(WIN32 AND NOT UNIX)
+IF(WIN32)
   # There is a bug in NSI that does not handle full unix paths properly. Make
   # sure there is at least one set of four (4) backlasshes.
   SET(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\qterm.exe")
   SET(CPACK_NSIS_HELP_LINK "http:\\\\\\\\www.qterm.org")
   SET(CPACK_NSIS_URL_INFO_ABOUT "http:\\\\\\\\www.qterm.org")
   SET(CPACK_NSIS_MODIFY_PATH ON)
-ELSE(WIN32 AND NOT UNIX)
+ELSEIF(APPLE)
+  SET(CPACK_GENERATOR "DragNDrop")
+ELSE()
   SET(CPACK_STRIP_FILES "bin/qterm")
   SET(CPACK_SOURCE_STRIP_FILES "")
-ENDIF(WIN32 AND NOT UNIX)
+ENDIF()
 
 # Functions to install qt, ssl libraries
 #FUNCTION(install_libs, lib
