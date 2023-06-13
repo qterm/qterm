7044f40f4515e693dd633d8348c1e5827aa67c27
sunzhu <sunzhu@gmail.com>
2023-06-13 15:06:22 +0800

    fix url identification

--- src/qtermbbs.cpp
+++ src/qtermbbs.cpp
@@ -389,7 +389,7 @@ bool BBS::isUnicolor(TextLine *line)
 bool BBS::isIllChar(QChar ch)
 {
     static QString illChars = ",;'\"()<>^[]";
-    return ch > '~' || ch < '#' || illChars.contains(ch);
+    return ch > '~' || (ch < '#' && ch != '!') || illChars.contains(ch);
 }
 
 bool BBS::isUrl(QRect& rcUrl, QRect& rcOld)
